4c0c5149be67f5fab7e10948cac3787e5cced67f
WilYoung <yy@y.local>
2016-01-21 14:33:37 +0800

    Initial Commit

---
+++ Minor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Minor
+//
+//  Created by yy on 16/1/21.
+//  Copyright © 2016年 yy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Minor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Minor
+//
+//  Created by yy on 16/1/21.
+//  Copyright © 2016年 yy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
